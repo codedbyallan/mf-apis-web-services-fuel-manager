383ea7955bd132cd12d60aff827983edb432b45f
Allan Barbosa <allan.barbosa@yahoo.com.br>
2026-03-31 01:18:24 -0300

    Iniciando a implementação de segurança da aplicação

--- mf-apis-web-services-fuel-manager/Models/AppDbContext.cs
+++ mf-apis-web-services-fuel-manager/Models/AppDbContext.cs
@@ -8,7 +8,32 @@ namespace mf_apis_web_services_fuel_manager.Models
         {
         }
 
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            // Define que a tabela VeiculoUsuarios terá chave composta
+            builder.Entity<VeiculoUsuarios>()
+                .HasKey(c => new {c.VeiculoId, c.UsuarioId });
+
+            // Define relacionamento: VeiculoUsuarios -> Veiculo
+            // Um VeiculoUsuarios tem um Veiculo
+            // Um Veiculo tem vários VeiculoUsuarios
+
+            builder.Entity<VeiculoUsuarios>()
+                .HasOne (c => c.Veiculo).WithMany(c => c.Usuarios)
+                .HasForeignKey(c => c.VeiculoId);
+
+            // Define relacionamento: VeiculoUsuarios->Usuario
+            // Um VeiculoUsuarios tem um Usuario
+            // Um Usuario tem vários VeiculoUsuarios
+
+            builder.Entity<VeiculoUsuarios>()
+                .HasOne(c => c.Usuario).WithMany(c => c.Veiculos)
+                .HasForeignKey(c => c.UsuarioId);
+        }
+
         public DbSet<Veiculo> Veiculos { get; set; }
         public DbSet<Consumo> Consumos { get; set; }
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<VeiculoUsuarios> VeiculoUsuarios { get; set; }
     }
 }
