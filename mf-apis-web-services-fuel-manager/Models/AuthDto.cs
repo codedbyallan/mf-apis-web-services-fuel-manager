db44f846ed9fa0160771786c8fc2e37355baaa27
Allan Barbosa <allan.barbosa@yahoo.com.br>
2026-04-01 04:43:13 -0300

    feat: segurança na API com autenticação JWT
    - Implementado login de usuários com validação de senha criptografada
    - Geração de token JWT com Claims (Id e Perfil)
    - Configuração do JwtBearer no Program.cs
    - Proteção dos controllers com [Authorize]
    - Endpoint de autenticação liberado com [AllowAnonymous]
    - Chave JWT movida para appsettings.json

---
+++ mf-apis-web-services-fuel-manager/Models/AuthDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace mf_apis_web_services_fuel_manager.Models
+{
+    public class AuthDto
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
--- mf-apis-web-services-fuel-manager/Program.cs
+++ mf-apis-web-services-fuel-manager/Program.cs
@@ -1,5 +1,8 @@
+using System.Text;
 using mf_apis_web_services_fuel_manager.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +13,29 @@ builder.Services.AddControllers()
 
 builder.Services.AddDbContext<AppDbContext>(options => 
     options.UseSqlServer(builder.Configuration.GetConnectionString("defaultConnection")));
+
+builder.Services.AddAuthentication(options =>
+    {
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+
+
+    //Adding Jwt Bearer
+    .AddJwtBearer(options =>
+    {
+        options.SaveToken = true;
+        options.RequireHttpsMetadata = false;
+        options.TokenValidationParameters = new TokenValidationParameters()
+        {
+            ValidateIssuerSigningKey = true,
+            ValidateIssuer = false,
+            ValidateAudience = false,
+            IssuerSigningKey = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(builder.Configuration["JWT:Secret"]))
+        };
+    });
 
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -25,7 +51,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
